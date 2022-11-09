a3d773b0a22b3e3ad1310b78927a6989c045e8a3
25th-Night <brown_eyed87@naver.com>
2022-11-09 17:03:02 +0900

    Add Custom Decorator to Check Login

--- users/views.py
+++ users/views.py
@@ -4,11 +4,23 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth import login, logout, authenticate
 from django.views.generic import FormView
 
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 from users.forms import LoginForm, SignUpForm
 from users.models import User
 # Create your views here.
 
 
+def my_decorator(function):
+    def decorator_func(request):
+        if not request.user.is_anonymous:
+            return redirect("main")
+        return function(request)
+
+    return decorator_func
+
+@method_decorator(my_decorator, name="get")
 class SignUpView(FormView):
     template_name = "users/sign_up.html"
     form_class = SignUpForm
@@ -32,6 +44,7 @@ class SignUpView(FormView):
     def form_invalid(self, form):
         return super().form_invalid(form)
     
+@method_decorator(my_decorator, name="get")
 class LoginView(FormView):
     """
     최선우 : form_valid 함수를 통해 로그인 요청 데이터의 인증 처리
