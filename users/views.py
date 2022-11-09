e2325488531d7054a2e23a7310f0ebf8dd408708
25th-Night <brown_eyed87@naver.com>
2022-11-09 16:55:32 +0900

    Add SignUpView in users/views.py

--- users/views.py
+++ users/views.py
@@ -4,13 +4,34 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth import login, logout, authenticate
 from django.views.generic import FormView
 
-from users.forms import LoginForm
+from users.forms import LoginForm, SignUpForm
+from users.models import User
 # Create your views here.
 
 
 class SignUpView(TemplateView):
     template_name = "users/sign_up.html"
+    form_class = SignUpForm
+    success_url = "/subscriptions/main/"
+    
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        
+        email = form.data.get("email")
+        fullname = form.data.get("fullname")
+        password = form.data.get("password")
 
+        user = User.objects.create_user(email, fullname, password)
+
+        user = authenticate(self.request, email=email, password=password)
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+    
 class LoginView(FormView):
     """
     최선우 : form_valid 함수를 통해 로그인 요청 데이터의 인증 처리
