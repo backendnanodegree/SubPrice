72cc04d5bf29000ccb12329e10a10e10b157bdd0
voonkvuno <voonkvuno@naver.com>
2022-11-15 09:33:12 +0900

    Add main view to get subscription table and expire table

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -11,7 +11,31 @@ from subscriptions.models import Billing, Company, Plan, Service, Subscription, 
 
 class MainListView(TemplateView):
     template_name = "subscriptions/main.html"
-    pass
+    
+    def get_context_data(self, **kwargs):
+
+        # display none header.html
+        context = {}
+        context['show_header'] = True
+
+        user = self.request.user
+        
+        # subscription table
+        subscription = Subscription.objects.filter(user=user, is_active=1).order_by("-started_at")
+        context['subscription_qs'] = subscription
+
+        # expire model
+        expire = Subscription.objects.filter(user=user, is_active=0).order_by("-expire_at")
+
+        if expire.count() >= 5 :
+            expire = expire[:5]
+        else:
+            expire_empty_row_count = 5 - expire.count()
+
+        context['expire_qs'] = expire
+        context['expire_empty_row_count'] = expire_empty_row_count
+
+        return context
 
 
 class MainCreateModalView(FormView):
