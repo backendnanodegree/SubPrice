85e79e7ff2cbc61d1be38eeef3bfb4511b831116
voonkvuno <voonkvuno@naver.com>
2022-11-15 09:08:52 +0900

    Fix main view, html, css

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import FormView
 from django.views.generic import TemplateView
+from django.core.paginator import Paginator
 
-# Create your views here.
+from alarms.models import Alarm
+
+from subscriptions.models import Billing, Company, Plan, Service, Subscription, Type
+
+
 class MainListView(TemplateView):
-    template_name = 'subscriptions/main.html'
+    template_name = "subscriptions/main.html"
     pass
+
