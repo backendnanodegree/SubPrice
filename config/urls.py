9a7944b2f3bad39a6cef720d8ce5e96b17e223f2
25th-Night <brown_eyed87@naver.com>
2022-12-07 01:25:52 +0900

    Add url path to user DJango Debug Toolbar

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('subscriptions/', include("subscriptions.urls")),    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('subscriptions/', include("subscriptions.urls")),
+    path("__debug__/", include(debug_toolbar.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
